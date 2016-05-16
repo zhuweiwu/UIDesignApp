8ce9050f75c05deb73943478774074ea2eb8d0d9
zhuweiwu <wuzhuweizzx@gmail.com>
2016-05-15 21:49:31 -0500

    remove test

--- app/src/main/java/com/android/zhuweiwu/uidesignapp/Test.java
+++
@@ -1,7 +0,0 @@
-package com.android.zhuweiwu.uidesignapp;
-
-/**
- * Created by zhuweiwu on 5/15/2016.
- */
-public class Test {
-}
--- myuilibrary/src/main/java/com/android/zhuweiwu/myuilibrary/Test.java
+++
@@ -1,8 +0,0 @@
-package com.android.zhuweiwu.myuilibrary;
-
-/**
- * Created by zhuweiwu on 5/15/2016.
- */
-public class Test {
-
-}
